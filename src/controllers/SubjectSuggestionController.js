bbd0df440d2b9e92beb17266ed51affa20928d0e
Femeuc <femeuf5@gmail.com>
2021-08-08 11:27:23 -0300

    esqueci vírgula

--- src/controllers/SubjectSuggestionController.js
+++ src/controllers/SubjectSuggestionController.js
@@ -4,7 +4,7 @@ module.exports = {
     async getAllSuggestions(req, res) {
         const response = await pool.query("SELECT * FROM subject_suggestion");
         res.status(200).json({response: response.rows});
-    }
+    },
 
     async createSuggestion(req, res) { 
         const response = await pool.query("INSERT INTO subject_suggestion (subject, subject_simplified, is_general_subject, channel) VALUES ($1, $2, $3, $4) RETURNING id", [
