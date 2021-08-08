98e8364d261aed2e6e36a40cee4d7e2eff2dbaa7
Femeuc <femeuf5@gmail.com>
2021-08-08 11:14:44 -0300

    mais organizado

--- src/controllers/QuestionSuggestController.js
+++ src/controllers/QuestionSuggestController.js
@@ -1,6 +1,11 @@
 const pool = require('../database/connection');
 
 module.exports = {
+    async getAllSuggestions(req, res) {
+        const response = await pool.query("SELECT * FROM suggestions");
+        res.status(200).json({response: response.rows[0]});
+    },
+    
     async createSuggestion(req, res) { 
         const response = await pool.query("INSERT INTO suggestions (description, option_a, option_b, option_c, option_d, correct_option, difficulty, subject, author) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id", [
             req.body.description,
@@ -14,10 +19,5 @@ module.exports = {
             req.body.author.toLowerCase()
         ]);
         res.status(200).json({response: response.rows[0].id});
-    }, 
-
-    async getAllSuggestions(req, res) {
-        const response = await pool.query("SELECT * FROM suggestions");
-        res.status(200).json({response: response.rows[0]});
     }
 };
