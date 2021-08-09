cfeb28fbb44400539cc7e97af00c4cb6e57f8aad
Femeuc <femeuf5@gmail.com>
2021-08-09 12:47:31 -0300

    rota para obter todas as justificativas.

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,7 @@ routes.put('/question/:id', QuestionsController.updateQuestion);
 routes.delete('/question/:id', QuestionsController.deleteQuestion);
 
 // Question Explanation Controller
+routes.get('/questions/explanations', QuestionExplanationController.getAllExplanations);
 routes.post('question/explanation', QuestionExplanationController.createExplanation);
 
 // Question Subjects Controller routes
